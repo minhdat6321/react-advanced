445376a01271081dd0bd32131cf877aca97dc6cd
minhdat6312 <minhdat6321@gmail.com>
2024-07-23 16:33:24 -0700

    Axios related to get/response/request data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react'
+import apiService from './app/apiService'
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const res = await axios.get('https://reqres.in/api/users/3')
+        const res = await apiService.post('/api/register', {
+          "email": "eve.holt@reqres.in",
+          "password": "pistol"
+        })
+        console.log(res)
+
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
